e589b361775886546a7c41dc2a2ab49da7f1d301
manuelhruggeri-ui <manuel.h.ruggeri@gmail.com>
2026-05-29 14:07:21 -0300

    Update dolar.js

--- netlify/functions/dolar.js
+++ netlify/functions/dolar.js
@@ -3,13 +3,9 @@ exports.handler = async (event) => {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json'
   };
-
   try {
-    const fetch = (await import('node-fetch')).default;
-    const resp = await fetch('https://dolarapi.com/v1/dolares/blue', {
-      headers: { 'User-Agent': 'Mozilla/5.0', 'Accept': 'application/json' }
-    });
-    if (!resp.ok) throw new Error(`DolarAPI responded ${resp.status}`);
+    const resp = await fetch('https://dolarapi.com/v1/dolares/blue');
+    if (!resp.ok) throw new Error(`DolarAPI error ${resp.status}`);
     const data = await resp.json();
     return { statusCode: 200, headers, body: JSON.stringify(data) };
   } catch (err) {
